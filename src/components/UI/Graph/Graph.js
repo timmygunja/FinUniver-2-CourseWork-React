5ab59e41eb6da9ba1ae8ec09f7121a798758d79b
timmygunja <t.urusov14@mail.ru>
2021-05-07 19:15:35 +0300

    Statistics graph now dynamically changes

--- src/components/UI/Graph/Graph.js
+++ src/components/UI/Graph/Graph.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { __DATA__ } from "./data";
+import React, {Component} from "react";
 import {
     MainContainer,
     Container,
@@ -8,22 +7,74 @@ import {
     BlackLine,
     MakeBar
 } from "./styles";
+import EmployeeService from "../../services/EmployeeService";
+import OrderService from "../../services/OrderService";
 
-export default function App() {
-    return (
-        <Container>
-            <MainContainer>
-                {__DATA__.map(({name, jobs, colors }, i) => {
-                    return (
-                        <BarChartContainer key={i}>
-                            <Number color={colors[1]}>{name} {jobs}</Number>
-                            <MakeBar height={jobs * 4} colors={colors} />
-                        </BarChartContainer>
-                    );
-                })}
-            </MainContainer>
-            <BlackLine />
-            <h5 className={"text-center m-4 text-black-60"}>Number of orders completed</h5>
-        </Container>
-    );
+
+class Graph extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: [],
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        EmployeeService.getEmployees().then((response) => {
+            this.setState({employees: response.data})
+
+            this.state.employees.map(
+                emp => {
+
+                    let count = 0
+
+                    OrderService.getOrders().then((response) => {
+                        const lst = response.data
+
+                        for (var i = 0; i < lst.length; i++) {
+                            // eslint-disable-next-line no-unused-expressions
+                            emp.surname === lst[i].employee.surname ? count++ : null
+                        }
+
+                        this.setState((prevState, props) => ({
+                            data: [
+                                ...prevState.data,
+                                {
+                                    name: emp.surname,
+                                    jobs: count,
+                                    colors: ["#52d98d", "#1da890"]
+                                }
+                            ]
+                        }))
+
+                    })
+                    return null
+                }
+            )
+
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <MainContainer>
+                    {this.state.data.map(({name, jobs, colors }, i) => {
+                        return (
+                            <BarChartContainer key={i}>
+                                <Number color={colors[1]}>{name} {jobs}</Number>
+                                <MakeBar height={jobs * 4} colors={colors} />
+                            </BarChartContainer>
+                        );
+                    })}
+                </MainContainer>
+                <BlackLine />
+                <h5 className={"text-center m-4 text-black-60"}>Number of orders completed</h5>
+            </Container>
+        );
+    }
 }
+
+
+export default Graph
--- src/components/UI/Graph/data.js
+++ src/components/UI/Graph/data.js
@@ -1,17 +1,8 @@
-export const __DATA__ = [
-    {
-        name: "Ivanov",
-        jobs: 5,
-        colors: ["#ffd847", "#e0a106"]
-    },
-    {
-        name: "Petrov",
-        jobs: 2,
-        colors: ["#ff47ab", "#e0064e"]
-    },
-    {
-        name: "Sidorov",
-        jobs: 7,
-        colors: ["#add9c0", "#1da890"]
-    },
-];
+import OrderService from "../../services/OrderService";
+import EmployeeService from "../../services/EmployeeService";
+
+
+
+// export default function App() {
+
+// }
